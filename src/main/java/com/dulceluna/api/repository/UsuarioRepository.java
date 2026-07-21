51d750a53ee3e189c10a8e70eb5a6e71ddff4796
Dalia Montserrat Caballero Silva <dalia.caballero.montserrat.silva@gmail.com>
2026-07-21 00:48:52 -0600

    Agregar entidad y repositorio de usuarios

--- src/main/java/com/dulceluna/api/repository/UsuarioRepository.java
+++ src/main/java/com/dulceluna/api/repository/UsuarioRepository.java
@@ -1,4 +1,17 @@
 package com.dulceluna.api.repository;
 
-public class UsuarioRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dulceluna.api.entity.Usuario;
+
+@Repository
+public interface UsuarioRepository
+        extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmailIgnoreCase(String email);
+
+    boolean existsByEmailIgnoreCase(String email);
 }
